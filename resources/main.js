4d129e8b04ee439201163389147bccc9967554a2
Lino Jimenez <linojr+linocodes@gmail.com>
2017-08-18 21:00:31 -0500

    restructured index html and main css

--- resources/main.js
+++ resources/main.js
@@ -1,15 +1,15 @@
-var upArrow = document.getElementById('up-arrow');
-
-var scrollUp = function() {
-  var y = window.scrollY;
-  if (y >= 600) {
-    upArrow.className = "scroll-top show"
-  } else {
-    upArrow.className = "bottomMenu hide"
-  }
-};
-
-window.addEventListener("scroll", scrollUp);
-upArrow.addEventListener("click", function(){
-  document.body.scrollTop = document.documentElement.scrollTop = 0;  
-})
+// var upArrow = document.getElementById('up-arrow');
+//
+// var scrollUp = function() {
+//   var y = window.scrollY;
+//   if (y >= 600) {
+//     upArrow.className = "scroll-top show"
+//   } else {
+//     upArrow.className = "bottomMenu hide"
+//   }
+// };
+//
+// window.addEventListener("scroll", scrollUp);
+// upArrow.addEventListener("click", function(){
+//   document.body.scrollTop = document.documentElement.scrollTop = 0;
+// })
